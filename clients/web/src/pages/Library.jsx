9b3e88b05f5f316a50c061849e322ab2c9f73db8
Abhishek3917 <abhishekskumar709@gmail.com>
2026-08-24 22:36:51 +0530

    web development

--- clients/web/src/pages/Library.jsx
+++ clients/web/src/pages/Library.jsx
@@ -38,7 +38,7 @@ const Library =()=>{
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
                     {media.map((item) => (
                         <MediaCard 
-                            key={item.id}
+                            key={item._id}
                             media={item}
                             onClick={() => navigate(`/watch/${item._id}`)}/>
                     ))}
